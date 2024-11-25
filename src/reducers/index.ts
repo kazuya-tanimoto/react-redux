035b72d2058e1b482f97b9f2dc6820a04297aa8c
Kazuya Tanimoto <tanimoto@byte-lark.com>
2024-11-25 09:42:45 +0900

    remove unneeded directories

--- src/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { counterReducer } from "./counter.ts";
-import { isLoginReducer } from "./isLogin.ts";
-
-export const reducers = combineReducers({
-  counter: counterReducer,
-  isLogin: isLoginReducer,
-});
--- src/reducers/isLogin.ts
+++
@@ -1,8 +0,0 @@
-export const isLoginReducer = (action = { type: "" }, state = false) => {
-  switch (action.type) {
-    case "LOGIN":
-      return !state;
-    default:
-      return state;
-  }
-};
